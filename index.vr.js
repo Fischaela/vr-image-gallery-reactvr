445077f8eb7e7d84ed9c2e8d49773ec4b6ddd491
Michaela Lehr <michaela.lehr@geildanke.com>
2017-02-16 14:43:12 +0100

    feat (light): add pointlight

--- index.vr.js
+++ index.vr.js
@@ -7,7 +7,8 @@ import {
   Text,
   View,
   Mesh,
-  AmbientLight
+  AmbientLight,
+  PointLight,
 } from 'react-vr';
 
 class GEILDANKE_REACTVR_GALLERY extends React.Component {
@@ -62,7 +63,7 @@ class GEILDANKE_REACTVR_GALLERY extends React.Component {
   render() {
     return (
       <View>
-        <AmbientLight intensity={ 5 }/>
+        <AmbientLight intensity={ 1.2 }/>
         <Mesh
           style={{
             transform: [
@@ -95,6 +96,18 @@ class GEILDANKE_REACTVR_GALLERY extends React.Component {
             ],
           }}
           source={{mesh:asset('cube_wall.obj'), mtl:asset('cube_wall.mtl'), lit: true}} />
+        <Mesh
+          style={{
+            transform: [
+              {translate: [0, -0.2, 0]},
+              {scale: [1, 0.001, 1]},
+            ],
+          }}
+          source={{mesh:asset('cube_wall.obj'), mtl:asset('cube_wall.mtl'), lit: true}} />
+        <PointLight intensity={0.25}
+          style={{
+            color:'#ffffff',
+            transform:[{translate : [0, 4, 0.25]}]}} />
       </View>
     );
   }
