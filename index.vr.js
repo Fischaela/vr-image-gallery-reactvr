522d73cf8d1a4c6e060d53b9938349eafb490d7f
Michaela Lehr <michaela.lehr@geildanke.com>
2017-02-17 14:32:53 +0100

    feat (sky): add cube_sky

--- index.vr.js
+++ index.vr.js
@@ -67,6 +67,13 @@ class GEILDANKE_REACTVR_GALLERY extends React.Component {
         <Mesh
           style={{
             transform: [
+              {scale: [1, 1, 1]},
+            ],
+          }}
+          source={{mesh:asset('cube_sky.obj'), mtl:asset('cube_sky.mtl'), lit: true}} />
+        <Mesh
+          style={{
+            transform: [
               {translate: [0, 0.2, -1]},
               {scale: [1, 0.4, 0.001]},
             ],
