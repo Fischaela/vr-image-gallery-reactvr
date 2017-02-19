2039ef635a4879122ffe42dbacf00b812f62b826
Michaela Lehr <michaela.lehr@geildanke.com>
2017-02-19 19:55:41 +0100

    chore (Button): add events with logs for testing to Button component

--- Button.js
+++ Button.js
@@ -10,8 +10,20 @@ import {
 
 class Button extends React.Component {
 
-  _onViewClicked() {
-    console.log('VrButton was clicked.');
+  _onButtonClicked() {
+    console.log('VrButton click.');
+  }
+
+  _onButtonEntered() {
+    console.log('VrButton enter.');
+  }
+
+  _onButtonExit() {
+    console.log('VrButton exit.');
+  }
+
+  _onButtonLongClick() {
+    console.log('VrButton long click.');
   }
 
   render() {
@@ -24,7 +36,10 @@ class Button extends React.Component {
 
     return(
       <VrButton
-        onClick={()=>this._onViewClicked()} >
+        onClick={()=>this._onButtonClicked()}
+        onEnter={()=>this._onButtonEntered()}
+        onExit={()=>this._onButtonExit()}
+        onLongClick={()=>this._onButtonLongClick()} >
         <Mesh
         style={{
           transform: [
